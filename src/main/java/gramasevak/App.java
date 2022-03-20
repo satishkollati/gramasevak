1d662aed849a2c4b937572d881c68882955c21e8
satish.kollati <satish.kollati@NE.com>
2022-03-20 18:53:35 +0530

    pusing v1 code

---
+++ src/main/java/gramasevak/App.java
@@ -0,0 +1,13 @@
+package gramasevak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
